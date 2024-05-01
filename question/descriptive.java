33c9e2754ee22247b0df6e91fa671af28b743ef7
Habbit-Tracker <eh.vandaki@gmail.com>
2024-05-01 13:28:46 +0330

    +add qustions full

--- question/descriptive.java
+++ question/descriptive.java
@@ -1,4 +1,18 @@
 package question;
 
+import java.util.Scanner;
+
 public class descriptive extends question{
+    private String answer ;
+
+    public descriptive(String title){
+        this.title = title;
+    }
+    public void SetAnswer(){
+        Scanner input = new Scanner(System.in) ;
+        answer = input.next();
+    }
+    public String getAnswer(){
+        return answer;
+    }
 }
