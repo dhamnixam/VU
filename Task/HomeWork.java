11661d0e184fe486055abfc5755dccd03f6debdf
mammadkhafan <sharifrazavian@gmail.com>
2024-05-01 13:02:27 +0430

    + Task package

---
+++ Task/HomeWork.java
@@ -0,0 +1,15 @@
+package Task;
+
+
+public class HomeWork extends Task{
+
+    public HomeWork(String subject, double start_time, double end_time) {
+        super(subject, start_time, end_time);
+    }
+
+    @Override
+    public String toString() {
+        return "Home work:\n" + "start at " + timeToString(start_time) + "\n" + "end at " + timeToString(end_time);
+    }
+    
+}
---
+++ Task/Quiz.java
@@ -0,0 +1,14 @@
+package Task;
+
+public class Quiz extends Task{
+
+    public Quiz(String subject, double start_time, double end_time) {
+        super(subject, start_time, end_time);
+    }
+
+    @Override
+    public String toString() {
+        return "Quiz:\n" + "start at " + timeToString(start_time) + "\n" + "end at " + timeToString(end_time);
+    }
+    
+}
